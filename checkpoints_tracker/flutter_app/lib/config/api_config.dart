b20a8e9d3c2c2be0a0f55cc25d8fcd73159b9386
asimfarooq5 <asim@xconn.io>
2026-07-15 20:57:14 +0500

    Update API config to server IP

--- checkpoints_tracker/flutter_app/lib/config/api_config.dart
+++ checkpoints_tracker/flutter_app/lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
   // Change this to your server's local network IP
-  static const String baseUrl = 'http://192.168.0.173:3300/api';
+  static const String baseUrl = 'http://91.99.147.23:3300/api';
   static const Duration timeout = Duration(seconds: 10);
 }
